6ed987f0545aba800fae4ba4110338ca43ae3376
joshuaengland03 <joshua.england1@students.snow.edu>
2025-01-29 15:58:04 -0700

    finished

--- ConsoleApp16/Program.cs
+++ ConsoleApp16/Program.cs
@@ -14,6 +14,7 @@ class Program
         Console.WriteLine("2. Cancel an order");
         Console.WriteLine("3. Track an order");
         Console.WriteLine("4. Exit");
+        
         string choice = Console.ReadLine();
 
         if (Enum.TryParse(choice, out MenuOptions menuOption))
@@ -49,6 +50,19 @@ class Program
         {
             Console.WriteLine("Invalid option. Please restart the program.");
         }
+        Console.WriteLine("How would you rate your experience from 1-10?");
+        string input = Console.ReadLine();
+        string thankYou = "";
+        bool result = int.TryParse(input, out int rating);
+        if (result && rating >= 1 && rating <=10)
+            {
+            thankYou = (rating > 7) ? "I'm glad you had a great experience! Have a nice day!" : "I'm sorry we could have done better. Have a nice day!";
+            }
+        else
+        {
+            Console.WriteLine("Error");
+        }
+        Console.WriteLine(thankYou);
     }
 
 
@@ -95,88 +109,3 @@ class Program
     }
 
 }
-
-
-
-
-
-
-//        if (choice == "1")
-//        {
-//            Console.WriteLine("You chose to place an order. What kind of product?");
-//            Console.WriteLine("Options: Electronics, Groceries, Clothing");
-//            string productType = Console.ReadLine();
-
-//            if (productType.ToLower() == "electronics")
-//            {
-//                Console.WriteLine("Order placed for Electronics!");
-//            }
-//            else if (productType.ToLower() == "groceries")
-//            {
-//                Console.WriteLine("Order placed for Groceries!");
-//            }
-//            else if (productType.ToLower() == "clothing")
-//            {
-//                Console.WriteLine("Order placed for Clothing!");
-//            }
-//            else
-//            {
-//                Console.WriteLine("Invalid product type.");
-//            }
-//        }
-//        else if (choice == "2")
-//        {
-//            Console.WriteLine("You chose to cancel an order.");
-//            Console.WriteLine("Enter the product type you want to cancel: Electronics, Groceries, Clothing");
-//            string cancelType = Console.ReadLine();
-
-//            if (cancelType.ToLower() == "electronics")
-//            {
-//                Console.WriteLine("Order for Electronics canceled.");
-//            }
-//            else if (cancelType.ToLower() == "groceries")
-//            {
-//                Console.WriteLine("Order for Groceries canceled.");
-//            }
-//            else if (cancelType.ToLower() == "clothing")
-//            {
-//                Console.WriteLine("Order for Clothing canceled.");
-//            }
-//            else
-//            {
-//                Console.WriteLine("Invalid product type.");
-//            }
-//        }
-//        else if (choice == "3")
-//        {
-//            Console.WriteLine("You chose to track an order.");
-//            Console.WriteLine("Enter the product type to track: Electronics, Groceries, Clothing");
-//            string trackType = Console.ReadLine();
-
-//            if (trackType.ToLower() == "electronics")
-//            {
-//                Console.WriteLine("Tracking information for Electronics.");
-//            }
-//            else if (trackType.ToLower() == "groceries")
-//            {
-//                Console.WriteLine("Tracking information for Groceries.");
-//            }
-//            else if (trackType.ToLower() == "clothing")
-//            {
-//                Console.WriteLine("Tracking information for Clothing.");
-//            }
-//            else
-//            {
-//                Console.WriteLine("Invalid product type.");
-//            }
-//        }
-//        else if (choice == "4")
-//        {
-//            Console.WriteLine("Exiting the system. Goodbye!");
-//        }
-//        else
-//        {
-//            Console.WriteLine("Invalid option. Please restart the program.");
-//        }
-//    }
-//}
